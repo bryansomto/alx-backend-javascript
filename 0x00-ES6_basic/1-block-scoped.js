bd54abd57e83f3f0ab20627f0a658f7caee714a4
bryansomto <bryansomto@gmail.com>
2022-12-20 00:41:51 +0100

    begun task 2

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,16 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  var task;
+  var task2;
+
+  task = false;
+  task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    let task;
+    let task2;
+
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
