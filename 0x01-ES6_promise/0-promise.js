9eb78e91857d68df09764e40634fff27109666fb
bryansomto <bryansomto@gmail.com>
2022-12-21 21:59:53 +0100

    modified task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,3 @@
 export default function getResponseFromAPI() {
-    const newPromise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('true'), reject('false');
-        }, 300);
-    });
-    return newPromise;
+    return new Promise((resolve, reject) => [resolve, reject]);
 }
