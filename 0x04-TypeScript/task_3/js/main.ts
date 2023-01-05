19906c01b40cdf702a9c4625e1ce3a14a3f80b34
bryansomto <bryansomto@gmail.com>
2023-01-05 04:18:41 +0100

    added all tasks

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,15 +1,15 @@
 
 import { RowID, RowElement } from './interface';
-import CRUD from './crud';
+import { insertRow, deleteRow, updateRow } from './crud';
 
 const row: RowElement = {
   firstName: 'Guillaume',
   lastName: 'Salva',
 }
 
-const newRowID: RowID = CRUD.insertRow(row);
+const newRowID: RowID = insertRow(row);
 row.age = 23;
 const updatedRow: RowElement = row;
 
-CRUD.updateRow(newRowID, updatedRow);
-CRUD.deleteRow(newRowID);
+updateRow(newRowID, updatedRow);
+deleteRow(newRowID);
